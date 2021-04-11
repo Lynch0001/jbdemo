8453c1da5ec3124f32d180ed88789786377068e3
Tim Lynch <Lynch0001@gmail.com>
2021-04-11 15:30:29 -0400

    Test

--- src/test/java/com/lynch/jbdemo/JbdemoApplicationTests.java
+++ src/test/java/com/lynch/jbdemo/JbdemoApplicationTests.java
@@ -1,11 +1,23 @@
 package com.lynch.jbdemo;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class JbdemoApplicationTests {
 
+	
+	
+	@Test
+	void sayhi() {
+		
+		JbdemoApplication jbdemo = new JbdemoApplication();
+		String actualResult = jbdemo.sayhi();
+		assertEquals("Hi!!!", actualResult);
+	}
+	
 	@Test
 	void contextLoads() {
 	}
