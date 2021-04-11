b271d099c91b5217c7396e7fdda375e0b3a9f7b1
Tim Lynch <Lynch0001@gmail.com>
2021-04-11 12:11:37 -0400

    Initial commit

---
+++ src/test/java/com/lynch/jbdemo/JbdemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lynch.jbdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JbdemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
